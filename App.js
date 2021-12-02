0a1b067b215666054da0e80524608ac20ed384c8
lavgin7 <oponeuser7@gmail.com>
2021-12-03 02:09:49 +0900

    fix tab size

--- App.js
+++ App.js
@@ -3,26 +3,26 @@ import { Text, View, Image, StyleSheet } from 'react-native';
 
 const app = () => {
   return (
-		<View style={styles.container}>
-			<Image 
-				source={require('./image/logo.png')} 
-				style={styles.title}
-			/>
-		</View>
+    <View style={styles.container}>
+      <Image 
+        source={require('./image/logo.png')} 
+        style={styles.title}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	title: {
-		width: 225,
-		height: 225,
-		resizeMode: "contain",
-	}
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    width: 225,
+    height: 225,
+    resizeMode: "contain",
+  }
 });
 
 export default app;
